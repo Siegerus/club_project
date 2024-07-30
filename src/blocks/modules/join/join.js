5ea5d432c834af50875afae363af631b792eb87d
Siegerus <ivanoffsiegerus@gmail.com>
2024-07-30 10:28:59 +0300

    promo+benefit+sign

---
+++ src/blocks/modules/join/join.js
@@ -0,0 +1,13 @@
+modules.define('join', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
