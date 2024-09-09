9461640960b8a3eb2ca325b090a13143eba84d09
Siegerus <ivanoffsiegerus@gmail.com>
2024-09-09 21:42:42 +0300

    event modal fin

---
+++ src/blocks/modules/events/events.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    let overlay = document.querySelector(".events__overlay"),
+        close = document.querySelector(".events__close"),
+        btn = document.querySelectorAll(".events__btn");
+
+
+
+    function showModal() {
+        btn.forEach((item) => {
+            item.addEventListener("click", function () {
+                overlay.style.display = "block";
+            });
+        });
+
+    }
+
+    showModal();
+
+    function closeModal() {
+        close.addEventListener("click", function() {
+            overlay.style.display = "none";
+        });
+    }
+
+    closeModal();
+
+    function closeByOverclick() {
+        overlay.addEventListener("click", function (e) {
+            let curentModal = e.target.closest(".modal");
+
+            if (!curentModal) {
+                overlay.style.display = "none";
+            }
+        });
+    }
+
+    closeByOverclick(); 
+});
