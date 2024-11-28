0b6740ccbc3001185247d1dc63dd29ab24efca60
Siegerus <ivanoffsiegerus@gmail.com>
2024-11-28 21:22:09 +0300

    IOSNavDone

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,20 +3,23 @@ window.addEventListener("DOMContentLoaded", function () {
 
     if (document.querySelector(".header")) {
 
-        let hamburger = document.querySelector(".header__hamburger"),
-            nav = document.querySelector(".header__nav_m-visible");
+        let hamburger = document.querySelectorAll(".header__hamburger"),
+            nav = document.querySelector(".header__nav_m-visible"),
+            navHamburger = nav.querySelector(".header__hamburger");
 
         let navShow = function () {
-            hamburger.addEventListener("click", function () {
-                this.classList.toggle("header__hamburger_active");
-                nav.classList.toggle("header__nav_active");
-                document.body.classList.toggle("body_noscroll");
-
-                if (nav.classList.contains("header__nav_active")) {
-                    document.body.classList.remove("body_eclipse");
-                } else if (document.body.querySelector(".action") || (document.body.querySelector(".connect"))) {
-                    document.body.classList.add("body_eclipse");
-                }
+            hamburger.forEach(function(item) {
+                item.addEventListener("click", function () {
+                    navHamburger.classList.toggle("header__hamburger_active");
+                    nav.classList.toggle("header__nav_active");
+                    document.body.classList.toggle("body_noscroll");
+    
+                    if (nav.classList.contains("header__nav_active")) {
+                        document.body.classList.remove("body_eclipse");
+                    } else if (document.body.querySelector(".action") || (document.body.querySelector(".connect"))) {
+                        document.body.classList.add("body_eclipse");
+                    }
+                });
             });
         };
 
