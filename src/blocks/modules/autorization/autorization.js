1f28968e3e4ca98758326db8cdc7e7c591190edd
Siegerus <ivanoffsiegerus@gmail.com>
2024-12-03 21:34:44 +0300

    SelectDone

--- src/blocks/modules/autorization/autorization.js
+++ src/blocks/modules/autorization/autorization.js
@@ -1,18 +1,20 @@
 window.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
-    let tabs = document.querySelectorAll(".autorization__tabs"),
-        content = document.querySelectorAll(".autorization__content"),
-        forget = document.querySelector(".autorization__forgetPass"),
-        block = document.querySelector(".autorization__block"),
-        blockPass = document.querySelector(".autorization__pass"),
-        select = document.querySelector(".feed-form__select"),
-        selectText = document.querySelector(".feed-form__select > span"),
-        optionsBlock = document.querySelector(".feed-form__optionsWrap"), 
-        options = optionsBlock.querySelectorAll(".feed-form__option"),
-        optionsRadio = optionsBlock.querySelectorAll(".feed-form__hidden");
-
     if (document.body.querySelector(".autorization")) {
+
+        let tabs = document.querySelectorAll(".autorization__tabs"),
+            content = document.querySelectorAll(".autorization__content"),
+            forget = document.querySelector(".autorization__forgetPass"),
+            block = document.querySelector(".autorization__block"),
+            blockPass = document.querySelector(".autorization__pass"),
+            select = document.querySelector(".feed-form__select_sign"),
+            selectText = document.querySelector(".feed-form__select > span"),
+            optionsBlock = document.querySelector(".feed-form__optionsWrap"), 
+            options = optionsBlock.querySelectorAll(".feed-form__option"),
+            optionsRadio = optionsBlock.querySelectorAll(".feed-form__hidden");
+
+    
         let contentHide = function (b) {
             for (let a = b; a < content.length; a++) {
                 content[a].classList.remove("show");
@@ -52,42 +54,43 @@ window.addEventListener("DOMContentLoaded", function () {
         };
 
         blockShow(forget, block, blockPass);
-    }
+    
 
-    let selectSet = function(clickItem) {
-        clickItem.addEventListener("click", function(e) {
-            if(e.target == (this)) {
-                select.classList.toggle("feed-form__select_active");
-                optionsBlock.classList.toggle("feed-form__optionsWrap_active");
-            }
-            
-            /* for(let i = 0; i < options.length; i++) {
-                if(e.target == options[i]) {
-                    optionsBlock.classList.remove("feed-form__optionsWrap_active");
-                    select.classList.remove("feed-form__select_active");
+        let selectSet = function(clickItem) {
+            clickItem.addEventListener("click", function(e) {
+                if(e.target == (this)) {
+                    select.classList.toggle("feed-form__select_sign_active");
+                    optionsBlock.classList.toggle("feed-form__optionsWrap_active");
                 }
-            } */
-        });
-
-        selectText.textContent = optionsRadio[0].value;
-        
-        optionsRadio.forEach((item, i) => {
-            item.addEventListener("click", () => {
-                if(optionsRadio[i].checked) {
-                    let val = optionsRadio[i].value;
-                    selectText.textContent = val;
+            
+                for(let i = 0; i < options.length; i++) {
+                    if(e.target == options[i]) {
+                        optionsBlock.classList.remove("feed-form__optionsWrap_active");
+                        select.classList.remove("feed-form__select_sign_active");
+                    }
                 }
             });
+
+            selectText.textContent = optionsRadio[0].value;
+            
+            optionsRadio.forEach((item, i) => {
+                item.addEventListener("click", () => {
+                    if(optionsRadio[i].checked) {
+                        let val = optionsRadio[i].value;
+                        selectText.textContent = val;
+                    }
+                });
+            });
+        };
+
+        document.body.addEventListener("click", (e) => {
+            if(e.target !== (select) && e.target !== (selectText)) {
+                optionsBlock.classList.remove("feed-form__optionsWrap_active");
+                select.classList.remove("feed-form__select_sign_active");
+            }
         });
-    };
 
-    document.body.addEventListener("click", (e) => {
-        if(e.target !== (select) && e.target !== (selectText)) {
-            optionsBlock.classList.remove("feed-form__optionsWrap_active");
-            select.classList.remove("feed-form__select_active");
-        }
-    });
-
-    selectSet(select);
-    selectSet(selectText);
+        selectSet(select);
+        selectSet(selectText);
+    }
 });
